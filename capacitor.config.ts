aa24a5c9e4ee52b3804eef8cf002589e4148d332
Julien Lecoq <julien_lecoq@protonmail.com>
2024-08-12 17:44:28 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test_watching_position',
+  webDir: 'www'
+};
+
+export default config;

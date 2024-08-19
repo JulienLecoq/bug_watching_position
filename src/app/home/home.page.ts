114a32013093968d77c29d93dce956b54ac5529b
Julien Lecoq <julien_lecoq@protonmail.com>
2024-08-19 16:13:34 +0200

    Add the bug

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone'
+import { Geolocation } from '@capacitor/geolocation'
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
+	standalone: true,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [
+		IonHeader,
+		IonToolbar,
+		IonTitle,
+		IonContent,
+	],
 })
 export class HomePage {
-  constructor() {}
+	ngOnInit() {
+		this.startWatchingPosition()
+	}
+
+	startWatchingPosition() {
+		Geolocation.watchPosition({}, (position) => {
+			console.log('Got position:', position)
+		})
+	}
 }
